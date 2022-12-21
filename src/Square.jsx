46978b28270ae63ca3ef5f224f67f2b636c14f7d
sebastian6551 <sebastian6551@live.com>
2022-12-21 01:53:26 -0500

    Organizo el código, las casillas cambian al hacer click

--- src/Square.jsx
+++ src/Square.jsx
@@ -2,48 +2,51 @@ import './styles/square.css';
 import horseIcon from './assets/knight.png';
 import bonusIcon from './assets/bonus.png';
 
-export const Square = ({ id, type, status, onClick = () => {} }) => {
-  const cursor = type === 'horse' ? 'pointer' : '';
+export const Square = ({id, type, status, onClick}) => {
 
-  const squareColor = (status) => {
-    switch (status) {
-      case 'free':
-        return '#E8EEF5';
-      case 'green':
-        return '#51C529';
-      case 'red':
-        return '#FF4547';
-      case 'free-dark':
-        return '#8AA7C9';
-      default:
-        return '#E8EEF5';
-    }
-  };
+    const cursor = type === 'horse' ? 'pointer' : '';
 
-  const showIcon = (type) => {
-    switch (type) {
-      case '':
-        return <></>;
-      case 'horse':
-        return <img className="squareIcon" alt="Hourse" src={horseIcon} />;
-      case 'bonus':
-        return <img className="squareIcon" alt="Hourse" src={bonusIcon} />;
-      default:
-        return <></>;
-    }
-  };
+    // Changes the color of the square according to its status.
+    const squareColor = (status) => {
+        switch (status) {
+            case 'free':
+                return '#E8EEF5';
+            case 'green':
+                return '#51C529';
+            case 'red':
+                return '#FF4547';
+            case 'free-dark':
+                return '#8AA7C9';
+            default:
+                return '#E8EEF5';
+        }
+    };
 
-  return (
-    <div
-      className="square"
-      onClick={() => onClick(id, type, status)}
-      style={{
-        background: squareColor(status),
-        border: '1px solid' + squareColor(status),
-        cursor: cursor,
-      }}
-    >
-      {showIcon(type)}
-    </div>
-  );
+    // Adds or removes an image to the square according to its type.
+    const showIcon = (type) => {
+        switch (type) {
+            case '':
+                return <></>;
+            case 'horse':
+                return <img className="squareIcon" alt="Hourse" src={horseIcon}/>;
+            case 'bonus':
+                return <img className="squareIcon" alt="Hourse" src={bonusIcon}/>;
+            default:
+                return <></>;
+        }
+    };
+
+    return (
+        <div
+            className="square"
+            onClick={onClick}
+            style={{
+                background: squareColor(status),
+                border: '1px solid' + squareColor(status),
+                cursor: cursor,
+            }}
+        >
+            {showIcon(type)}
+        </div>
+    );
 };
