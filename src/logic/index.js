7d8cdd9f6fedb7dd4bc92a3f04b1d06af0c187fa
JuanCamilo-R <juan.randazzo@correounivalle.edu.co>
2022-12-22 14:18:09 -0500

    Solucionando bug, se le pasa el mundo como matriz al algoritmo de profundidad

--- src/logic/index.js
+++ src/logic/index.js
@@ -1,7 +1,7 @@
 import { DepthAlgorithm } from "./depthAlgorithm.js";
-import { World } from "./world";
+import { World } from "./world.js";
 
 let newWorld = new World();
 newWorld.fillWorld();
-const algorithm = new DepthAlgorithm(newWorld, 1);
+const algorithm = new DepthAlgorithm(newWorld.getWorld(), 1);
 const solution = algorithm.start();
